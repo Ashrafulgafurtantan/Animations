4d227372d9bcf7e2ec00c337b6ee2949a866a6d8
Dexter <bsse1021@iit.du.ac.bd>
2020-06-27 10:51:54 +0600

    Animation full course

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:animationexample/widgets/cat.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -5,21 +6,58 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController animationController;
+  Animation<double> boxAnimation;
+  AnimationController boxanimationController;
 
-  buildAnimation(){
-    return Container(
-      height: 200,
-      width: 200,
-      decoration: new BoxDecoration(
-          image: new DecorationImage(
-            image: AssetImage('assets/images/cat.png'),
-            fit: BoxFit.contain,
-          )
-      ),
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    boxanimationController =AnimationController(
+      duration: Duration(milliseconds: 300),
+      vsync: this,
     );
-  }
 
+    boxAnimation = Tween(begin: 3.14*0.6,end: 3.14*0.65).animate(
+      CurvedAnimation(
+        curve: Curves.linear,
+        parent: boxanimationController,
+      ),
+    ) ;
+    boxAnimation.addStatusListener((status) {
+      if(status ==AnimationStatus.completed){
+        boxanimationController.reverse();
+      }
+      else if(status==AnimationStatus.dismissed){
+        boxanimationController.forward();
+      }
+    });
+    boxanimationController.forward();
+
+
+
+
+
+    animationController =AnimationController(
+      duration: Duration(milliseconds: 300),
+      vsync: this,
+    );
+
+    catAnimation = Tween(begin: -35.0,end: -80.0).animate(
+      CurvedAnimation(
+        curve: Curves.easeIn,
+        parent: animationController,
+      ),
+    ) ;
+
+
+
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +65,109 @@ class _HomeState extends State<Home> {
         title: Center(child: Text('Animation')),
 
       ),
-      body: buildAnimation(),
+      body: GestureDetector(
+        child: Center(
+          child: new Stack(
+            overflow: Overflow.visible,
+            children: <Widget>[
+
+              buildCatAnimation(),
+              buildBox(),
+
+              buildLeftFlep(),
+              buildRightFlep(),
+            ],
+          ),
+        ),
+
+        onTap: (){
+          boxanimationController.stop();
+        if(animationController.status == AnimationStatus.completed){
+          boxanimationController.forward();
+          animationController.reverse();
+
+        }
+        else if(animationController.status == AnimationStatus.dismissed){
+          animationController.forward();
+          boxanimationController.stop();
+
+
+        }
+      },),
+
+    );
+  }
+
+  Widget buildCatAnimation(){
+
+    return AnimatedBuilder(
+        animation: catAnimation, 
+        child: Cat(),
+
+        builder: (context,child){
+          return Positioned(
+            child: child,
+            top: catAnimation.value,
+            right: 0.0,
+            left: 0.0,
+          );
+        }
+
+
+    );
+  }
+
+
+  buildBox(){
+   return Container(
+     height: 200,
+    width: 200,
+     color: Colors.brown[300],
+   );
+  }
+  buildLeftFlep(){
+    return Positioned(
+      left: 3,
+      child: AnimatedBuilder(
+       animation: boxAnimation,
+          child: Container(
+            height: 10,
+            width: 125,
+            color: Colors.orangeAccent,
+          ),
+
+          builder:(context,child){
+            return Transform.rotate(
+              angle: boxAnimation.value,
+            child: child,
+              alignment: Alignment.topLeft,
+            );
+          }
+
+      ),
+    );
+  }
+
+  buildRightFlep(){
+    return Positioned(
+      right: 3,
+      child: AnimatedBuilder(
+          animation: boxAnimation,
+          child: Container(
+            height: 10,
+            width: 125,
+            color: Colors.orangeAccent,
+          ),
+
+          builder:(context,child){
+            return Transform.rotate(
+              angle: -boxAnimation.value,
+              child: child,
+              alignment: Alignment.topRight,
+            );
+          }
+
+      ),
     );
   }
 }
